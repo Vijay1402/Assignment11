0ab5fa3d38bd5df8ff5ac9ed46d11deed702dd8c
Vijayaraghavan R <rvrblr@gmail.com>
2023-11-13 14:58:46 +0530

    Assignment

---
+++ Assignment11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 0;
+            Salary obj = new Salary();
+            Console.WriteLine("CHoose one of the option to calculate salary:\n1.HR\n2.Admin\n3.Software Deveoloper");
+            int x = int.Parse(Console.ReadLine());
+            obj.inputDetails(x);
+            if (x==1)
+            {
+                salary = obj.calcSalary(obj.wHour, obj.wDays);
+            }
+            else if (x==2) 
+            {
+                salary = obj.calcSalary(obj.wHour, obj.wDays, obj.projectHandles);
+            }
+            else if (x == 3)
+            {
+                salary = obj.calcSalary(obj.wHour, obj.wDays, obj.projectHandles, obj.extras);
+            }
+            Console.WriteLine("The calculate salary is: Rs."+salary);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment11/Salary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+
+    public class Salary
+    {
+        public double wHour, wDays;
+        public int projectHandles, extras;
+        public void inputDetails(int x)
+        {
+            Console.WriteLine("Enter the no of working hours");
+            wHour = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the no of working days");
+            wDays = double.Parse(Console.ReadLine());
+            if (x > 1)
+            {
+                Console.WriteLine("Enter the no of project handles");
+                projectHandles = int.Parse(Console.ReadLine());
+            }
+            if (x > 2)
+            {
+                Console.WriteLine("Enter the no of extras");
+                extras = int.Parse(Console.ReadLine());
+            }
+        }
+        public double calcSalary(double wHour, double wDays, int projectHandles = 1, int extras = 0)
+        {
+            double salary = wHour * wDays * 100 + projectHandles * 3000 + extras * 2000;
+            return salary;
+        }
+    }
+}
